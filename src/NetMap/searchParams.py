d9e3293ecc8310d7685da3a82625cbd421ef90ef
Matthew Finch <finchrmatthew@gmail.com>
2021-06-04 00:56:39 -0400

    searchSuite

--- src/NetMap/searchParams.py
+++ src/NetMap/searchParams.py
@@ -1,12 +1,6 @@
 
 from NetMap import ( LocationServices, DateRange )
 
-        
-def set_locations(locations: list) -> dict:
-
-    return LocationServices(locations=locations).fetch(radius=4)
-
-
 def set_wordList():
 
     return [
@@ -15,6 +9,11 @@ def set_wordList():
         'covid19',
         'biden'
     ]
+        
+def set_locations(locations: list) -> dict:
+
+    return LocationServices(locations=locations).fetch(radius=4)
+
 
 def set_dates(start_date: str = None, num_days: int = 7):
 
@@ -22,7 +21,17 @@ def set_dates(start_date: str = None, num_days: int = 7):
 
 class SearchParams:
 
-    def __init__():
+    def __init__(self, locations: list, words: list, 
+                        start_date: str = None, num_days: int = 7):
 
-        pass
+        self.__locations = set_locations(locations=locations)
+        self.__dates = set_dates(start_date=start_date, num_days=num_days)
+
+    @property
+    def locations(self):
+        return self.__locations
+
+    @property
+    def dates(self):
+        return self.__dates
 
