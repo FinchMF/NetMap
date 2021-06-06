72ac976d22093dbe27816ca21d902773573dc78d
Matthew Finch <finchrmatthew@gmail.com>
2021-06-05 22:03:45 -0400

    reorg

--- src/NetMap/__init__.py
+++ src/NetMap/__init__.py
@@ -23,13 +23,13 @@ from tweepy import ( API, Cursor, OAuthHandler, TweepError, RateLimitError )
 from typing import ( List, Dict, Any, Union, Optional, TypeVar )
 
 import NetMap.config as cfg
-from NetMap.searchWords import WordSearch
-from NetMap.searchCoordinates import LocationServices
-from NetMap.searchDates import DateRange
-from NetMap.searchParams import SearchParams
+from NetMap.search.searchWords import WordSearch
+from NetMap.search.searchCoordinates import LocationServices
+from NetMap.search.searchDates import DateRange
+from NetMap.search.searchParams import SearchParams
 from NetMap.utils import *
-from NetMap.dataTypes import *
-from NetMap.dataModels import *
+from NetMap.data.dataTypes import *
+from NetMap.data.dataModels import *
 from NetMap.twBot import *
 
 ##################################
