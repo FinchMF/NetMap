08f282549201868bf9aa2c96e322b9db79a50b70
Matthew Finch <finchrmatthew@gmail.com>
2021-08-04 23:55:36 -0400

    favicon added

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
-from flask import ( Flask, render_template, request, url_for )
+from flask import ( Flask, render_template, request, url_for, send_from_directory )
 from NetMap import ( LINE, plotly )
 import json
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico') 
+def favicon(): 
+    return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
@@ -10,30 +14,21 @@ def index():
 
 @app.route('/loading', methods=['POST'])
 def loading():
-    print('made it')
-    try:
-        print(request.json)
-        print(type(request.json))
-        params: dict = {
 
-            'words': [str(request.json['q'])],
-            'locations': [str(request.json['location'])],
-            'start_date': '2021-08-02',
-            'num_days': 7
-        } 
+    params: dict = {
 
-        records: int = 1 # try more variables / or create security around number of locations and words
-        pipeline = LINE(params=params, records=records)
-        pipeline.getData()
-        pipeline.send2db()
-        data = pipeline.callData()
-        data = pipeline.dataFramed(data)
-        net = pipeline.generateNetwork(data=data, title='Accounts <-> Hashtags')
-        graphJSON = json.dumps(net, cls=plotly.utils.PlotlyJSONEncoder)
-        return graphJSON
-    except Exception as e:
-        print(e)
-    
-    return {}
+        'words': [str(request.json['q'])],
+        'locations': [str(request.json['location'])],
+        'start_date': '2021-08-02',
+        'num_days': 7
+    } 
 
-    
+    records: int = 1 # try more variables / or create security around number of locations and words
+    pipeline = LINE(params=params, records=records)
+    pipeline.getData()
+    pipeline.send2db()
+    data = pipeline.callData()
+    data = pipeline.dataFramed(data)
+    net = pipeline.generateNetwork(data=data, title='Accounts <-> Hashtags')
+    graphJSON = json.dumps(net, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON    
